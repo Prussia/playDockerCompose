5876454b847bde9546fcba82a48c8431bbee6949
Prussia <prussia.hu@gmail.com>
2017-01-07 20:20:11 +0800

    data source file

--- deploy_conf.sh
+++ deploy_conf.sh
@@ -1,9 +1,14 @@
 #! /bash/sh
 
 docker cp ./tomcat-users.xml playspring_tomcat_1:/usr/local/tomcat/conf/
+
+docker cp ./content.xml playspring_tomcat_1:/usr/local/tomcat/conf/
+
 if test -e "probe.war"; then 
 	echo "prob found"
 else 
+	echo "downloading start"
 	wget "https://github.com/psi-probe/psi-probe/releases/download/3.0.0.RC1/probe.war";
 fi
+
 docker cp ./probe.war playspring_tomcat_1:/usr/local/tomcat/webapps/
