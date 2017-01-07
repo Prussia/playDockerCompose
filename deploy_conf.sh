fbee888660f571ab79a556e617ed470441a256b9
Prussia <prussia.hu@gmail.com>
2017-01-07 16:35:40 +0800

    update container name

--- deploy_conf.sh
+++ deploy_conf.sh
@@ -1,3 +1,3 @@
-docker cp ./tomcat-users.xml playSpring_tomcat_1:/usr/local/tomcat/conf/
+docker cp ./tomcat-users.xml playspring_tomcat_1:/usr/local/tomcat/conf/
 wget "https://github.com/psi-probe/psi-probe/releases/download/3.0.0.RC1/probe.war"
-docker cp ./probe.war /usr/local/tomcat/webapps/
+docker cp ./probe.war playspring_tomcat_1:/usr/local/tomcat/webapps/
